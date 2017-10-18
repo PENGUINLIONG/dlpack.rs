10cfc9dfec184cd559a75d16ada3ec992f213d1f
PENGUINLIONG <admin@penguinliong.moe>
2017-10-18 22:16:41 +0800

    Finishing touch

--- src/lib.rs
+++ src/lib.rs
@@ -3,48 +3,50 @@
 ///! https://github.com/dmlc/dlpack
 use std::os::raw::c_void;
 
-pub type DeviceType = u32;
-pub mod device_type {
-    use super::DeviceType;
+pub type DeviceTypeCode = i32;
+pub mod device_type_codes {
+    use super::DeviceTypeCode;
 
-    pub const CPU:        DeviceType = 1;
-    pub const GPU:        DeviceType = 2;
-    pub const CPU_PINNED: DeviceType = 3;
-    pub const OPENCL:     DeviceType = 4;
-    pub const METAL:      DeviceType = 8;
-    pub const VPI:        DeviceType = 9;
-    pub const ROCM:       DeviceType = 10;
+    pub const CPU:        DeviceTypeCode = 1;
+    pub const GPU:        DeviceTypeCode = 2;
+    pub const CPU_PINNED: DeviceTypeCode = 3;
+    pub const OPENCL:     DeviceTypeCode = 4;
+    pub const METAL:      DeviceTypeCode = 8;
+    pub const VPI:        DeviceTypeCode = 9;
+    pub const ROCM:       DeviceTypeCode = 10;
 }
 
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct Context {
-    device_type: DeviceType,
-    device_id: i32,
+    pub device_type: DeviceTypeCode,
+    pub device_id: i32,
 }
 
 pub type DataTypeCode = u8;
-pub mod data_type_code {
-    pub const INT: u8 = 0;
-    pub const UINT: u8 = 1;
-    pub const FLOAT: u8 = 2;
+pub mod data_type_codes {
+    use super::DataTypeCode;
+
+    pub const INT:   DataTypeCode = 0;
+    pub const UINT:  DataTypeCode = 1;
+    pub const FLOAT: DataTypeCode = 2;
 }
 
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct DataType {
-    code: DataTypeCode,
-    bits: u8,
-    lanes: u16,
+    pub code: DataTypeCode,
+    pub bits: u8,
+    pub lanes: u16,
 }
 
 #[repr(C)]
 pub struct Tensor {
-    data: *mut c_void,
-    ctx: Context,
-    ndim: i32,
-    dtype: DataType,
-    shape: *mut i64,
-    strides: *mut i64,
-    byte_offset: u64,
+    pub data: *mut c_void,
+    pub ctx: Context,
+    pub ndim: i32,
+    pub dtype: DataType,
+    pub shape: *mut i64,
+    pub strides: *mut i64,
+    pub byte_offset: u64,
 }
